3c710e86ca0178494fd312790fc191a3cb72302d
Alex392 <43678045+Alex392@users.noreply.github.com>
2018-10-29 14:49:34 -0400

    Add files via upload

---
+++ USER_INFO.py
@@ -0,0 +1,44 @@
+import praw
+import time
+import sys
+
+#this is the code that will retrive a Redditor's USER INFORMATION
+#Such as: USERNAME; REDDIT ID; DATE THE ACCOUNT WAS CREATED;E-MAIL VERIFICATION
+
+
+reddit = praw.Reddit(client_id = 'pPciqPn--wkB9Q' ,
+                     client_secret = 'pX5mKEZUUP2QP7w4bux1haeBDuc' ,
+                     username = 'nullbreakers-1',
+                     password ='DJKehoe',
+                     user_agent ='testapp')
+
+print(reddit.user.me())
+
+
+userSearch = raw_input("Who are you looking for?")#askes the user who they are looking for
+try:
+#	if reddit.redditor(name=userSearch).has_verified_email:
+#		print('the user exists')
+	USERNAME = reddit.redditor(name=userSearch).name
+	REDDIT_ID = reddit.redditor(name=userSearch).id
+	rB_DAY = time.strftime("%Y-%m-%d",time.localtime(reddit.redditor(name=userSearch).created_utc))
+	EMAIL = reddit.redditor(name=userSearch).has_verified_email
+		
+#	else: 
+#		print('You need varify your e-mail')
+#		USERNAME = reddit.redditor(name=userSearch).name
+#		REDDIT_ID = reddit.redditor(name=userSearch).id
+#		rB_DAY = time.strftime("%Y-%m-%d",time.localtime(reddit.redditor(name=userSearch).created_utc))
+#		EMAIL = reddit.redditor(name=userSearch).has_verified_email
+except :
+	print('the account is avable')
+	sys.exit()
+#	print(reddit.redditor(name=userSearch).name)
+#	print(reddit.redditor(name=userSearch).id)
+#	print(reddit.redditor(name=userSearch).created_utc)
+#	print(reddit.redditor(name=userSearch).has_verified_email)
+
+print('USERNAME: ' + USERNAME)
+print('REDDIT_ID: ' + REDDIT_ID)
+print('rB_DAY: ' + rB_DAY)
+print('EMAIL: ' + str(EMAIL))
