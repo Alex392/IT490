e5fd31ba20125c86e6e6d9419a6bc5e425daa203
Dan Valinotti <dvalinotti@icloud.com>
2018-11-07 18:00:17 -0500

    added script for auto-posting to reddit

--- cron.php
+++ cron.php
@@ -1,10 +1,34 @@
 #!/usr/bin/php
 <?php
-    $file = fopen('autopost.csv', 'r');
-    while (($line = fgetcsv($file)) !== FALSE) {
-      print_r($line);
 
-      exec("python SUBREDDIT_POST.py '".$line[0]."' '".$line[1]."' '".$line[2]."'" ,$output,$result);
-    }
+function new_campaign_entry($subreddit, $subject, $content, $hour)
+{
+    $date = date('Y-m-d\TH:i:sP', time());
+    $entry = array ($subreddit, $subject, $content, $hour, $date);
+
+    $file = fopen('autopost.csv', 'a');
+    fputcsv($file, $entry);
     fclose($file);
+}
+new_campaign_entry('TheTestLife','Test','Bob',1);
+
+$file = fopen('autopost.csv', 'r');
+
+while (($line = fgetcsv($file)) !== false) {
+    print_r($line);
+
+    $date = date('Y-m-d\TH:i:sP', time());
+    $date = date_create($date);
+    $date2 = date_create($line[4]);
+    echo gettype($date2);
+    $diff = $date->diff($date2);
+
+    $hours = $diff->h;
+
+    if($hours >= $line[3]) {
+        echo exec("python SUBREDDIT_POST.py '".$line[0]."' '".$line[1]."' '".$line[2]."'", $output, $result);
+    }
+
+}
+fclose($file);
 ?>
